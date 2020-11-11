7f1560b4e7f861fb325e876e62ce2855d89856b8
Kyle Barlow <kbarlowjs@gmail.com>
2020-11-11 12:46:40 -0700

    initial commit

---
+++ content.js
@@ -0,0 +1,23 @@
+var source = document.getElementsByTagName("html")[0].innerHTML;
+
+var foundPayingAttention = source.indexOf("paying attention");
+var foundPleaseSelectUpper = source.indexOf("Please Select");
+var foundPleaseSelectLower = source.indexOf("please select");
+
+
+
+document.addEventListener('keydown', function(event) {
+  if (event.key === '`') {
+
+
+    if (foundPayingAttention !== -1) {
+      alert("paying attention");
+    } else if (foundPleaseSelectUpper !== -1) {
+      alert("please select");
+    } else if (foundPleaseSelectLower !== -1) {
+      alert("please select");
+    }
+
+
+  }
+});
---
+++ readme.txt
@@ -0,0 +1,6 @@
+1. Scan HTML for multiple strings
+    If (strings are found in the HTML) {
+        2. Alert
+        3. Highlight words
+        4. Navigate to word
+    }
